789a018e2109bf415c637fcdc15b5e630c5a01ff
Andrew Meserole <andrew.meserole@rackspace.com>
2016-07-29 13:04:16 -0500

    Added exit upon fail flags to both scripts

--- osa-aio.sh
+++ osa-aio.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 apt-get update
 
 apt-get install -y vim git tmux
--- rpc-aio.sh
+++ rpc-aio.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 apt-get update
 
 apt-get install -y vim git tmux
@@ -7,6 +9,9 @@ apt-get install -y vim git tmux
 git clone --recursive https://github.com/rcbops/rpc-openstack /opt/rpc-openstack
 
 pushd /opt/rpc-openstack
+
+	#git checkout liberty-12.2
+	#git submodule update
 
 	export DEPLOY_AIO="yes"
 
